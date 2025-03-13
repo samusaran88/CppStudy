0697d4b1540759209b21228f5176f036e1a1aa6b
SamusAran <neitt@naver.com>
2025-03-13 20:19:18 +0900

    add

---
+++ CppStudy21_클래스/CppStudy21/MainGame.cpp
@@ -0,0 +1,11 @@
+#include "MainGame.h"
+//생성자
+MainGame::MainGame()
+{
+	cout << "생성 완료!!" << endl;
+}
+//소멸자
+MainGame::~MainGame()
+{
+	cout << "파괴 완료!!" << endl;
+}
---
+++ CppStudy21_클래스/CppStudy21/MainGame.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class MainGame
+{
+public:
+	MainGame();	//생성자 : 객체가 생성됨과 동시에 자동으로 실행되는 곳
+	~MainGame();//소멸자 : 객체가 소멸(종료)됨과 동시에 자동으로 실행되는 곳
+};
+
